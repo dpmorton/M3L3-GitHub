8ae39533ac8d60b2b7dff8b0172b3c63a44962ad
David Morton <dpmorton@gmail.com>
2022-11-21 18:06:39 -0500

    Initial Commit

---
+++ M3L3-GitHub/M3L3_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3L3_GitHubApp.swift
+//  M3L3-GitHub
+//
+//  Created by dpm on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct M3L3_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
