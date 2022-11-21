06e70b2136497da8201246f54ad8ccc9cb7f5458
dpmorton <118759110+dpmorton@users.noreply.github.com>
2022-11-21 18:25:19 -0500

    Update ContentView.swift

--- M3L3-GitHub/ContentView.swift
+++ M3L3-GitHub/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, My fellow coders")
+            Text("What's up, My fellow coders?")
         }
         .padding()
     }
